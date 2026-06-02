5366b96273bdf114c57d9b54d758c87a425a0cc2
Yuriy Mishchuk <yuramishchuk1703@gmail.com>
2026-06-03 02:22:03 +0300

    hotfixe5

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,7 @@ output "vm_name_uppercase" {
 
 
 output "tags_joined" {
-  value = [for vm in azurerm_virtual_machine.main : vm.id]
+  value = join(", ", values(azurerm_virtual_machine.main[0].tags))
 }
 
 
